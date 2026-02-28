7f7e14877288ba4f0a5db38672993bfd43e39e44
Georgiy Grobovoy <gro-jorge@mail.ru>
2026-02-28 12:40:12 +0300

    hw03 spring boot and localization

---
+++ hw03/src/main/java/ru/otus/hw/service/TestServiceImpl.java
@@ -0,0 +1,62 @@
+package ru.otus.hw.service;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+import ru.otus.hw.dao.QuestionDao;
+import ru.otus.hw.domain.Answer;
+import ru.otus.hw.domain.Question;
+import ru.otus.hw.domain.Student;
+import ru.otus.hw.domain.TestResult;
+
+@Service
+@RequiredArgsConstructor
+public class TestServiceImpl implements TestService {
+
+    private final LocalizedIOService ioService;
+
+    private final QuestionDao questionDao;
+
+    @Override
+    public TestResult executeTestFor(Student student) {
+        ioService.printLine("");
+        ioService.printLineLocalized("TestService.answer.the.questions");
+        ioService.printLine("");
+
+        var questions = questionDao.findAll();
+        var testResult = new TestResult(student);
+
+        for (var question: questions) {
+            var isAnswerValid = askQuestion(question);
+            testResult.applyAnswer(question, isAnswerValid);
+        }
+        return testResult;
+    }
+
+    private Boolean askQuestion(Question question) {
+        ioService.printLine(question.text());
+        ioService.printLine("");
+
+        int answerNumber = 1;
+        for (Answer answer : question.answers()) {
+            ioService.printFormattedLine("%d. %s", answerNumber, answer.text());
+            answerNumber++;
+        }
+
+        ioService.printLine("");
+
+        int choiceNumber = ioService.readIntForRangeWithPromptLocalized(1, answerNumber, "TestService.enter.choice", "TestService.invalid.choice");
+
+        return isAnswerCorrectForQuestion(choiceNumber, question);
+    }
+
+    private Boolean isAnswerCorrectForQuestion(int choiceNumber, Question question) {
+        var answer = question.answers().get(choiceNumber - 1);
+
+        if (answer.isCorrect()) {
+            return true;
+        }
+
+        return false;
+    }
+}
