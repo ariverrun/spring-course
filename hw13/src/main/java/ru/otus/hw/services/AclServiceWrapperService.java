f572c867089a0e101a1f5749b104a13c63c5ba86
Georgiy Grobovoy <gro-jorge@mail.ru>
2026-05-05 17:17:46 +0300

    rm unneeded methods

--- hw13/src/main/java/ru/otus/hw/services/AclServiceWrapperService.java
+++ hw13/src/main/java/ru/otus/hw/services/AclServiceWrapperService.java
@@ -6,11 +6,5 @@ public interface AclServiceWrapperService {
 
     void createPermission(Object object, Permission permission);
     
-    void createPermission(Object object, Permission permission, String username);
-    
-    void createPermissionForRole(Object object, Permission permission, String role);
-    
-    void deletePermission(Object object, Permission permission);
-    
     void deleteAcl(Object object);
 }
--- hw13/src/main/java/ru/otus/hw/services/AclServiceWrapperServiceImpl.java
+++ hw13/src/main/java/ru/otus/hw/services/AclServiceWrapperServiceImpl.java
@@ -1,13 +1,11 @@
 package ru.otus.hw.services;
 
-import org.springframework.security.acls.domain.GrantedAuthoritySid;
 import org.springframework.security.acls.domain.ObjectIdentityImpl;
 import org.springframework.security.acls.domain.PrincipalSid;
 import org.springframework.security.acls.model.MutableAcl;
 import org.springframework.security.acls.model.MutableAclService;
 import org.springframework.security.acls.model.ObjectIdentity;
 import org.springframework.security.acls.model.Permission;
-import org.springframework.security.acls.model.Sid;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
@@ -25,35 +23,7 @@ public class AclServiceWrapperServiceImpl implements AclServiceWrapperService {
     @Transactional
     public void createPermission(Object object, Permission permission) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        createPermissionInternal(object, permission, new PrincipalSid(authentication));
-    }
-
-    @Override
-    @Transactional
-    public void createPermission(Object object, Permission permission, String username) {
-        createPermissionInternal(object, permission, new PrincipalSid(username));
-    }
-
-    @Override
-    @Transactional
-    public void createPermissionForRole(Object object, Permission permission, String role) {
-        createPermissionInternal(object, permission, new GrantedAuthoritySid(role));
-    }
-
-    @Override
-    @Transactional
-    public void deletePermission(Object object, Permission permission) {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        deletePermissionInternal(object, permission, new PrincipalSid(authentication));
-    }
-
-    @Override
-    @Transactional
-    public void deleteAcl(Object object) {
-        mutableAclService.deleteAcl(new ObjectIdentityImpl(object), false);
-    }
-
-    private void createPermissionInternal(Object object, Permission permission, Sid sid) {
+        var sid = new PrincipalSid(authentication);
         ObjectIdentity oid = new ObjectIdentityImpl(object);
         
         MutableAcl acl;
@@ -67,15 +37,9 @@ public class AclServiceWrapperServiceImpl implements AclServiceWrapperService {
         mutableAclService.updateAcl(acl);
     }
 
-    private void deletePermissionInternal(Object object, Permission permission, Sid sid) {
-        ObjectIdentity oid = new ObjectIdentityImpl(object);
-        MutableAcl acl = (MutableAcl) mutableAclService.readAclById(oid);
-        
-        acl.getEntries().stream()
-            .filter(ace -> ace.getPermission().equals(permission) && ace.getSid().equals(sid))
-            .findFirst()
-            .ifPresent(ace -> acl.deleteAce(acl.getEntries().indexOf(ace)));
-        
-        mutableAclService.updateAcl(acl);
+    @Override
+    @Transactional
+    public void deleteAcl(Object object) {
+        mutableAclService.deleteAcl(new ObjectIdentityImpl(object), false);
     }
 }
